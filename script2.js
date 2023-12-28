ce047430f8bbffd75d1d39ec1e8c94aa2d0285c5
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-12-28 19:04:58 +0100

    Great one

--- script2.js
+++ script2.js
@@ -5,3 +5,26 @@ const addEntryButton = document.getElementById("add-entry")
 const clearButton = document.getElementById("clear")
 const output = document.getElementById("output")
 let isError = false
+
+// Although inputs fields are number types, javascript receives them as strings
+// hence the need to convert them to numbers
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
+// exponential "e" is also allowed in HTML number input, hence this function
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+function addEntry() {
+    //This code dynamically picks the html element based of the category selected
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+
+    //Inside the t
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder = "Name" id="${entryDropdown.value}-${entryNumber}-name"
+    `;
+  }
